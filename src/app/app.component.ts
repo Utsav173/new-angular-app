6da4f0869ae3e9186850250c18df7e0df0f02685
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2022-07-29 20:05:28 +0530

    more done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'new-angular-app';
+
+
+export class AppComponent implements OnInit {
+  title = 'this is title';
+  apidata:any=[];
+  async ngOnInit(): Promise<void> {
+    try{
+      const response = await fetch("https://inshorts.deta.dev/news?category=", {
+        "method": "GET",
+        "headers": {}
+      });
+    
+      if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+        this.apidata = result.data;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    console.log(this.apidata);
+    
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
